1489fd9b20dce93072397e88b671fff55665957f
Johnnybar <johnnybareket@gmail.com>
2018-02-28 19:53:57 +0100

    additional style and color changes

--- src/contact.js
+++ src/contact.js
@@ -16,13 +16,13 @@ export default class Contact extends React.Component{
         return(
             <div id='contact-section'>
 
-                <form className='contact-form-container paint-font-black' target="_blank"  action="mailto:johnnybareket@gmail.com" method="post" enctype="text/plain" >
-                <br/><div className='paint-font-black'>Wanna work together? Like what you see? Did I gain some weight? Let me know!</div><br/>
+                <form className='contact-form-container' target="_blank"  action="mailto:johnnybareket@gmail.com" method="post" enctype="text/plain" >
+                <br/><div>Wanna work together? Like what you see? Did I gain some weight? Let me know!</div><br/>
                 Full Name: <input type="text" name="FullName" placeholder='Full Name'></input>
                     Email:<input type="text" name="Email" placeholder='Email Address'></input>
                         Subject:<input type="text" name="Subject" placeholder='Subject'></input>
                         <textarea id= 'text-area-contact' placeholder="Let's hear it!"></textarea>
-                        <input className='click-btn' type="submit" name="submit" value="Submit"></input>
+                        <input className='click-btn' type="submit" name="submit" value="Submit" id='button'></input>
                 </form>
 
             </div>
--- src/work.js
+++ src/work.js
@@ -18,13 +18,14 @@ export default class Work extends React.Component{
             <div id='work-section'>
                 <div className='work-top'>
                 <div className='work-title'><h1 >My Work So Far</h1></div>
-                <div className='work-description'><p> <a href='https://github.com/Johnnybar/imageboard' target="_blank">Image Bunker</a>, An Instagram-like website where users can upload images and comment on other users' images.
-                <a href='https://github.com/Johnnybar/social-network' target="_blank"> <br/>Track#Share</a>,  Social Network where users can register/login, befriend other users, see who's online and exchange song suggestions on the community chat.
-                <a href='https://github.com/Johnnybar/petition'> Save The Drop Bear</a>, an online petition allowing users to register, manage a profile, sign a petition and see signers.
-                 <a href='https://github.com/Johnnybar/english-showtimes-berlin' target="_blank"> English Showtimes Berlin</a>, a website that allows users to view cinemas that play non-dubbed movies in Berlin, check the current program using an API,
+                <div className='work-description'><p> <a href='https://imagebunker.herokuapp.com/#/home' target="_blank">Image Bunker</a>, An Instagram-like website where users can upload images and comment on other users' images.
+                <a href='http://track-share.herokuapp.com/welcome/#/' target="_blank"> <br/>Track#Share</a>,  Social Network where users can register/login, befriend other users, see who's online and exchange song suggestions on the community chat.
+                <a href='https://j-petition.herokuapp.com/register'> Save The Drop Bear</a>, an online petition allowing users to register, manage a profile, sign a petition and see signers.
+                 <a href='http://english-showtimes-berlin.herokuapp.com' target="_blank"> English Showtimes Berlin</a>, a website that allows users to view cinemas that play non-dubbed movies in Berlin, check the current program using an API,
                  save favourite cinemas and search for movie info using the IMDB DB.
                 <a href='https://github.com/Johnnybar/mini-projects/tree/master/spotifyApiSearch' target="_blank"> SpotifyUi</a>, a mini-Spotify UI to search for and play music from albums and artists.
-                <a href='https://github.com/Johnnybar/mini-projects/tree/master/GithubApiSearch' target="_blank"> GitSearch.</a>, a mini-app, designed to search for GitHub users and view their most recent commits.
+                <a href='https://github.com/Johnnybar/mini-projects/tree/master/GithubApiSearch' target="_blank"><br/> GitSearch.</a>, a mini-app, designed to search for GitHub users and view their most recent commits.
+                <br/>View source code for all projects <a href='https://github.com/Johnnybar' target="_blank">here.</a>
              </p>
                 </div>
                 </div>
